2554a54b7194eae6bacd084f7797c67c18501a1d
Martin Labryga <martin@labryga.ch>
2019-09-30 18:26:49 +0200

    build_sh to build development renamed

--- docker/build_development.sh
+++ docker/build_development.sh
@@ -1,3 +1,8 @@
-export host_user_name=$(id -un) host_user_id=$(id -u)
+
+export host_user_name=$(id -un) \
+       host_user_id=$(id -u)
+
 cd ../django/
 . build_development_docker.sh
+
+cd ../gulp/
