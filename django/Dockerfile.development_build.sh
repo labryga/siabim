3b1f40312cea50876ba9788529201cf8d6c5ce1b
Martin Labryga <martin@labryga.ch>
2019-10-01 21:22:08 +0200

    dockerfile specification changed

--- django/Dockerfile.development_build.sh
+++ django/Dockerfile.development_build.sh
@@ -1,4 +1,5 @@
+
 docker image build -t siabim_django:2.2.5 \
   --build-arg host_user_name=$(id -un) \
   --build-arg host_user_id=$(id -u) \
-  -f ./development.Dockerfile .
+  -f ./Dockerfile.development .
