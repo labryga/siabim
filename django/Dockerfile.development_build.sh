ade8dc774dd03564f53c57fc17bb3d8291344091
Martin Labryga <martin@labryga.ch>
2019-10-03 10:09:40 +0200

    gulp dockerfile indentation fixed

--- django/Dockerfile.development_build.sh
+++ django/Dockerfile.development_build.sh
@@ -1,5 +1,6 @@
 
-docker image build -t siabim_django:2.2.5 \
+docker image build \
+  -t siabim_django:2.2.5 \
   --build-arg host_user_name=$(id -un) \
   --build-arg host_user_id=$(id -u) \
   -f ./Dockerfile.development .
