a428fecd44f7742d6de67c345f623ef3a929d86d
Martin Labryga <martin@labryga.ch>
2019-10-25 10:42:51 +0200

    eval testing function evaluated

--- staticfiles/js/development/siabim_es6_build.js
+++ staticfiles/js/development/siabim_es6_build.js
@@ -1,3 +1,9 @@
-// alert("ja");
+
+const mytime = new Date();
+
+const mytimes = ["getFullYear", "getHours", "getMinutes"];
+
+mytimes.forEach(item => 
+  console.log(eval("mytime" + "." + item + "()")));
 
 
--- staticfiles/js/production/siabim.js
+++ staticfiles/js/production/siabim.js
@@ -1,2 +1,15 @@
-// alert("ja");
 "use strict";
+
+var mytime = new Date();
+var mytimes = ["getFullYear", "getHours", "getMinutes"];
+var _a = mytimes;
+
+var _f = function _f(item) {
+  return console.log(eval("mytime" + "." + item + "()"));
+};
+
+for (var _i = 0; _i < _a.length; _i++) {
+  _f(_a[_i], _i, _a);
+}
+
+undefined;
