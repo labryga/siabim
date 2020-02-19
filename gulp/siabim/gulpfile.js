ec162fe31af2ccba502e231884507f36733a5fd8
Martin Labryga <martin@labryga.ch>
2020-02-19 21:05:58 +0100

    normalise.css put on top of order in gulp configuration

--- gulp/siabim/gulpfile.js
+++ gulp/siabim/gulpfile.js
@@ -12,8 +12,9 @@ var gulp          = require('gulp'),
 
 gulp.task('css', () => {
   return gulp.src([
+    '/staticfiles/css/development/*.css',
     '/staticfiles/css/development/sass/SiaBimTestpage.sass',
-    '/staticfiles/css/development/*.css',])
+    ])
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer('last 5 versions',
                        'Android >= 2.3',
