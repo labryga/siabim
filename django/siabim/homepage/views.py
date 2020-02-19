d4824b5721c0e93a8e057e68b4ff2702139cce37
Martin Labryga <martin@labryga.ch>
2020-02-19 20:06:34 +0100

    SiaBimTestpage view added

--- django/siabim/homepage/views.py
+++ django/siabim/homepage/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import TemplateView
 
+
 class SiaBimHomepage(TemplateView):
     template_name = "SiaBimHomepage.pug"
 
+
+class SiaBimTestpage(TemplateView);
+    template_name = "SiaBimTestpage.pug"
--- django/siabim/siabim/urls.py
+++ django/siabim/siabim/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from homepage.views import SiaBimHomepage
 
+
 urlpatterns = [
     path("", SiaBimHomepage.as_view()),
     path("admin/", admin.site.urls),
