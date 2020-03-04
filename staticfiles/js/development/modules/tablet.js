5d881ee7a680bcdb997ad6c89cc6063fa8f5ed73
labryga <marcin@labryga.ch>
2020-03-04 17:31:01 +0100

    tablat js added to keep footer at bottom depending on content height

--- staticfiles/js/development/modules/tablet.js
+++ staticfiles/js/development/modules/tablet.js
@@ -1,15 +1,13 @@
-"use strict";
-
-console.log("TestPage");
 var fensterhoehe = window.innerHeight;
-var inhalthoehe = grid_container_tablet.clientHeight;
+var inhalthoehe  = grid_container_tablet.clientHeight;
 
 if (inhalthoehe > fensterhoehe) {
   console.log("inhalt gross");
   grid_container_tablet.style.height = "";
-  footer_tablet.style.position = "";
+  footer_tablet.style.position       = "";
+
 } else {
   console.log("inhalt klein");
   grid_container_tablet.style.height = "100%";
-  footer_tablet.style.position = "absolute";
+  footer_tablet.style.position       = "absolute";
 }
--- staticfiles/js/development/siabim_es6_build.js
+++ staticfiles/js/development/siabim_es6_build.js
@@ -1,1 +1,16 @@
 console.log("TestPage");
+
+
+var fensterhoehe = window.innerHeight;
+var inhalthoehe  = grid_container_tablet.clientHeight;
+
+if (inhalthoehe > fensterhoehe) {
+  console.log("inhalt gross");
+  grid_container_tablet.style.height = "";
+  footer_tablet.style.position       = "";
+
+} else {
+  console.log("inhalt klein");
+  grid_container_tablet.style.height = "100%";
+  footer_tablet.style.position       = "absolute";
+}
