cee67e41c0f19207708293f8d6f864e74eb2cc26
Martin Labryga <martin@labryga.ch>
2020-03-10 18:32:52 +0100

    var to let changed in phone.js

--- staticfiles/js/development/modules/phone.js
+++ staticfiles/js/development/modules/phone.js
@@ -1,5 +1,5 @@
-var fensterhoehe = window.innerHeight;
-var inhalthoehe  = grid_container_phone.clientHeight;
+let fensterhoehe = window.innerHeight;
+let inhalthoehe  = grid_container_phone.clientHeight;
 
 
 if (inhalthoehe > fensterhoehe) {
--- staticfiles/js/development/siabim_es6_build.js
+++ staticfiles/js/development/siabim_es6_build.js
@@ -15,8 +15,8 @@ if (inhalthoehe > fensterhoehe) {
 }
 
 
-var fensterhoehe = window.innerHeight;
-var inhalthoehe  = grid_container_phone.clientHeight;
+let fensterhoehe = window.innerHeight;
+let inhalthoehe  = grid_container_phone.clientHeight;
 
 
 if (inhalthoehe > fensterhoehe) {
