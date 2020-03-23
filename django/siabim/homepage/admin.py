d0a1ef68360d564057f7ab03dc6ac9661d3f2f44
Martin Labryga <martin@labryga.ch>
2020-03-23 23:04:08 +0100

    HomepageModelAdmin registerd

--- django/siabim/homepage/admin.py
+++ django/siabim/homepage/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import HomepageModel
 
-# Register your models here.
+
+class HomepageModelAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(HomepageModel, HomepageModelAdmin)
