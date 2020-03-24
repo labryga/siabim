5f95ff418d2d865346a1b3898e3f1602bf80d39b
Martin Labryga <martin@labryga.ch>
2020-03-24 18:59:10 +0100

    nginx custom image added

--- nginx/Dockerfile.development_build.sh
+++ nginx/Dockerfile.development_build.sh
@@ -1,5 +1,5 @@
 docker image build \
-  -t siabim_django:2.2.11 \
+  -t siabim_nginx:1.17 \
   --build-arg host_user_name=$(id -un) \
   --build-arg host_user_id=$(id -u) \
   -f ./Dockerfile.development .
