f18cc1d60901d2d3c9952932acf06e1e148b05d5
Martin Labryga <martin@labryga.ch>
2020-03-28 23:09:45 +0100

    str method removed

--- django/siabim/homepage/models.py
+++ django/siabim/homepage/models.py
@@ -5,7 +5,3 @@ from ckeditor.fields import RichTextField
 class HomepageModel(models.Model):
     title = models.CharField(max_length=200, blank=True, null=True)
     text = RichTextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.title
-
