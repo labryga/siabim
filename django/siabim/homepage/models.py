c35caecc821ba8922a6198744c066085c9cf558a
Martin Labryga <martin@labryga.ch>
2020-03-28 23:54:40 +0100

    __str__ method added

--- django/siabim/homepage/models.py
+++ django/siabim/homepage/models.py
@@ -5,3 +5,6 @@ from ckeditor.fields import RichTextField
 class HomepageModel(models.Model):
     title = models.CharField(max_length=200, blank=True, null=True)
     text = RichTextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
