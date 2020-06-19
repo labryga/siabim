f4929be29176d20cdf24c7b29d5c81e654537c4e
Martin Labryga <martin@labryga.ch>
2020-06-19 19:17:44 +0200

    django 2.2.13 update

--- django/Dockerfile.development_build.sh
+++ django/Dockerfile.development_build.sh
@@ -1,6 +1,6 @@
 
 docker image build \
-  -t siabim_django:2.2.11 \
+  -t siabim_django:2.2.13 \
   --build-arg host_user_name=$(id -un) \
   --build-arg host_user_id=$(id -u) \
   -f ./Dockerfile.development .
