45ef39f47cc2dae41fdc5244cc900ef2d1e06c3e
Marcin Labryga <marcin@labryga.com>
2020-09-07 13:10:22 +0200

    node 12.18.3 update

--- gulp/Dockerfile.development_build.sh
+++ gulp/Dockerfile.development_build.sh
@@ -1,6 +1,5 @@
-
 docker image build \
-  -t siabim_gulp:node_10.16.3 \
+  -t siabim_gulp:node_12.18.3 \
   --build-arg host_uid=$(id -u) \
   --build-arg host_uid_name=$(id -un) \
   -f ./Dockerfile.development .
